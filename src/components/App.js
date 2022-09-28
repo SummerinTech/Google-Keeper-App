bca5dcb64c23a943bda27785c85a4154b4b379cd
Summer <72228259+SummerinTech@users.noreply.github.com>
2022-09-27 18:55:17 -0700

    add Note component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Header";
+import Note from "./Note";
 import Footer from "./Footer";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<Note />
 			<Footer />
 		</div>
 	);
