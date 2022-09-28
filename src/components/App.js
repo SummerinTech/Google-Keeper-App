3cb0b31fd42dd9c5048b100742546c2cc0db4d20
Summer <72228259+SummerinTech@users.noreply.github.com>
2022-09-27 18:43:15 -0700

    set up starting files for project
    Added different components for the app and styled the header and footer components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<Header />
+			<Footer />
 		</div>
 	);
 }
